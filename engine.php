c9d2dd6e466ac3bb73d4baf94050bb7d4c396045
Damian Dygulski <dygulski13@gmail.com>
2017-03-13 18:42:37 +0100

    Modules listing added

--- engine.php
+++ engine.php
@@ -1,6 +1,20 @@
 <?php
   include('./lib/php/siBase.php');
+
+  $_MODULES = array();
+
   function loadModule($name){
     include('./modules/'.$name.'.php');
+  }
+  function listModules(){
+    $query = sql('SELECT `name` FROM :table WHERE `enabled` = :enabled', 'modules', array('enabled' => 1));
+    if($query){
+      foreach($query as $q){
+        $_MODULES = $q['name'];
+      }
+    }
+    else{
+      die('error');
+    }
   }
 ?>
