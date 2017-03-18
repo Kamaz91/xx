e973d084260962480f1d5fa8768714b3ae7763ce
Damian Dygulski <dygulski13@gmail.com>
2017-03-18 21:00:22 +0100

    Semi-drunk help not needed.

--- modules/main.php
+++ modules/main.php
@@ -1,16 +1,20 @@
 <?php
   if(isset($_SESSION['ID'])){
-    print '<div class="news">';
-
+    print '<div class="stats">';
     print '</div>';
-    print '<div class="battles">';
+    print '<div class="main">';
+      print '<div class="news">';
 
-    print '</div>';
-    print '<div class="shout">';
+      print '</div>';
+      print '<div class="battles">';
 
-    print '</div>';
-    print '<div class="quickNews">';
+      print '</div>';
+      print '<div class="shout">';
 
+      print '</div>';
+      print '<div class="quickNews">';
+
+      print '</div>';
     print '</div>';
   }
   else{
