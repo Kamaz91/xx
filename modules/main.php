845cd2aba263aa81ac807e8b0b9996320d60b805
Damian Dygulski <dygulski13@gmail.com>
2017-03-18 20:17:01 +0100

    I have no idea what I'm doing. Send help

--- modules/main.php
+++ modules/main.php
@@ -1,6 +1,17 @@
 <?php
   if(isset($_SESSION['ID'])){
-    print 'Welcome to Main';
+    print '<div class="news">';
+
+    print '</div>';
+    print '<div class="battles">';
+
+    print '</div>';
+    print '<div class="shout">';
+
+    print '</div>';
+    print '<div class="quickNews">';
+
+    print '</div>';
   }
   else{
     include('modulesHTML/loginError.html');
