3134b9df90d3573e17909cf875ee694e0b643e2b
Damian Dygulski <dygulski13@gmail.com>
2017-03-21 17:25:59 +0100

    CSS changes

--- index.php
+++ index.php
@@ -9,20 +9,21 @@
 
   include('modulesHTML/menu.html');
   print '<div class="container">';
-  if(maintenanceCheck() == false){
-    if(isset($_GET['module'])){
-      loadModule($_GET['module']);
-    }
-    else{
-      if(isset($_SESSION['ID'])){
-        loadModule('main');
+    print '<div class="matrioshka">';
+      if(maintenanceCheck() == false){
+        if(isset($_GET['module'])){
+          loadModule($_GET['module']);
+        }
+        else{
+          if(isset($_SESSION['ID'])){
+            loadModule('main');
+          }
+          else{
+            loadModule('login');
+          }
+        }
       }
-      else{
-        loadModule('login');
-      }
-    }
-  }
-
+    print '</div>';
   include('modulesHTML/footer.html');
   print '</div>';
 ?>
