3f7bbb71d11ea1266a864370f17f8318b9a2bef8
Damian Dygulski <dygulski13@gmail.com>
2017-03-26 18:54:40 +0200

    XP Gain with training

--- modules/train.php
+++ modules/train.php
@@ -1,16 +1,17 @@
 <?php
-  $data = sql('SELECT `str`,`train` FROM :table WHERE `id` = :id','users',array('id'=>$_SESSION['ID']));
+  $data = sql('SELECT `str`,`train`,`xp` FROM :table WHERE `id` = :id','users',array('id'=>$_SESSION['ID']));
   if($data){
     $str = $data[0]['str'];
     if($data[0]['train'] == 0 && isset($_POST['train'])){
       $maxstr = sql('SELECT `str` FROM :table ORDER BY `str` DESC LIMIT 1','users')[0]['str'];
       $ratio = $maxstr / $str;
-      $base = calculateStr($ratio, 50);
-      $str += $base;
-      $up = sql('UPDATE :table SET `str` = :str, `train` = :tr WHERE `id` = :id','users',array(
+      $xp = $data[0]['xp'] + 5;
+      $str += calculateStr($ratio, 50);
+      $up = sql('UPDATE :table SET `str` = :str, `train` = :tr, `xp` = :xp WHERE `id` = :id','users',array(
         'id' => $_SESSION['ID'],
         'str' => $str,
-        'tr' => 1
+        'tr' => 1,
+        'xp' => $xp
       ));
       if($up){
         loadHTML('templates/trained',array(
