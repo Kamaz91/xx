a6decbd31947d8fc56f26566a667cd065b6b9bfa
Damian Dygulski <dygulski13@gmail.com>
2017-04-20 00:00:33 +0200

    Some optimisation

--- engine.php
+++ engine.php
@@ -1,55 +1,55 @@
 <?php
-  set_include_path(__DIR__);
+	set_include_path(__DIR__);
 
-  include('lib/php/siBase.php');
+	include('lib/php/siBase.php');
 
-  session_start();
+	session_start();
 
-  $_MODULES = array();
-  $_SETTINGS = array();
+	$_MODULES = array();
+	$_SETTINGS = array();
 
-  function error($n = 'error.html'){
-    include('modulesHTML/error/'.$n);
-  }
-  function loadModule($name){
-    global $_MODULES;
-    if(in_array($name, $_MODULES)){
-      include('modules/'.$name.'.php');
-    }
-    else{
-      error();
-    }
-  }
-  function listModules(){
-    global $_MODULES;
-    if(!empty($_MODULES)){
-      $_MODULES = array();
-    }
-    $query = sql('SELECT `name` FROM :table WHERE `enabled` = :enabled', 'modules', array('enabled' => 1));
-    if($query){
-      foreach($query as $q){
-        $_MODULES[] = $q['name'];
-      }
-    }
-    else{
-      error();
-    }
-  }
-  function loadSettings(){
-    global $_SETTINGS;
-    if(!empty($_SETTINGS)){
-      $_SETTINGS = array();
-    }
-    $query = sql('SELECT `name`, `value` FROM :table WHERE 1', 'settings'); // WHERE 1...
-    if($query){
-      foreach($query as $q){
-        $_SETTINGS[$q['name']] = $q['value'];
-      }
-    }
-    else{
-      error();
-    }
-  }
+	function error($n = 'error.html'){
+		include('modulesHTML/error/'.$n);
+	}
+	function loadModule($name){
+		global $_MODULES;
+		if(in_array($name, $_MODULES)){
+			include('modules/'.$name.'.php');
+		}
+		else{
+			error();
+		}
+	}
+	function listModules(){
+		global $_MODULES;
+		if(!empty($_MODULES)){
+			$_MODULES = array();
+		}
+		$query = sql('SELECT `name` FROM :table WHERE `enabled` = :enabled', 'modules', array('enabled' => 1));
+		if($query){
+			foreach($query as $q){
+				$_MODULES[] = $q['name'];
+			}
+		}
+		else{
+			error();
+		}
+	}
+	function loadSettings(){
+		global $_SETTINGS;
+		if(!empty($_SETTINGS)){
+			$_SETTINGS = array();
+		}
+		$query = sql('SELECT `name`, `value` FROM :table WHERE 1', 'settings'); // WHERE 1...
+		if($query){
+			foreach($query as $q){
+				$_SETTINGS[$q['name']] = $q['value'];
+			}
+		}
+		else{
+			error();
+		}
+	}
 	function loadUserData($id){
 		$query = sql('SELECT `nick`,`usrgroup`,`country`,`xp`,`lvl`,`eco`,`str`,`food`,`med`,`damage` FROM :table WHERE `id` = :id','users',array(
 			'id' => $id
@@ -64,62 +64,63 @@
 			error();
 		}
 	}
-  function maintenanceCheck(){
-    $query = sql('SELECT `value` FROM :table WHERE `name` = :name','settings',array(
-      'name' => 'maintenance'
-    ));
-    if($query){
-      if($query[0]['value'] == 1){
-        error('maintenance.html');
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-    else{
-      error();
-    }
-  }
-  function loadTemplate($file, $data = array(), $return = false){
-    $fdata = file_get_contents('modulesHTML/'.$file.'.html', true);
-    if(!empty($data)){
-      foreach($data as $key => $value){
-        $tag = "[@$key]";
-        $fdata = str_replace($tag,$value,$fdata);
-      }
-      if($return == true){
-        return $fdata;
-      }
-      else{
-        echo $fdata;
-      }
-    }
-    else{
-      if($return == true){
-        return $fdata;
-      }
-      else{
-        echo $fdata;
-      }
-    }
-  }
-  function calculateStr($ratio = 1, $base = 50){
-    if($ratio === 1){
-      return $base;
-    }
-    elseif($ratio > 1 && $ratio < 2){
-      return $base *= 2;
-    }
-    elseif($ratio >= 2 && $ratio < 3){
-      return $base *= 3;
-    }
-    elseif($ratio >= 3){
-      return $base *= 4;
-    }
-  }
-  function loadShout(){
-    // Table is there... some refining needed with the template but works
+	function maintenanceCheck(){
+		$query = sql('SELECT `value` FROM :table WHERE `name` = :name','settings',array(
+			'name' => 'maintenance'
+		));
+		if($query){
+			if($query[0]['value'] == 1){
+				error('maintenance.html');
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			error();
+		}
+	}
+	function loadTemplate($file, $data = array(), $return = false){
+		$fdata = file_get_contents('modulesHTML/'.$file.'.html', true);
+		if(!empty($data)){
+			$tags = array();
+			foreach($data as $key => $value){
+				$tags[] = '[@'.$key.']';
+			}
+			$fdata = str_replace($tags,$data,$fdata);
+			if($return == true){
+				return $fdata;
+			}
+			else{
+				echo $fdata;
+			}
+		}
+		else{
+			if($return == true){
+				return $fdata;
+			}
+			else{
+				echo $fdata;
+			}
+		}
+	}
+	function calculateStr($ratio = 1, $base = 50){
+		if($ratio === 1){
+			return $base;
+		}
+		elseif($ratio > 1 && $ratio < 2){
+			return $base *= 2;
+		}
+		elseif($ratio >= 2 && $ratio < 3){
+			return $base *= 3;
+		}
+		elseif($ratio >= 3){
+			return $base *= 4;
+		}
+	}
+	function loadShout(){
+		// Table is there... some refining needed with the template but works
 		$shouts = sql('SELECT `content`,`author`,`time` FROM :table LIMIT 10','shouts');
 		$shoutHTML = '';
 		if($shouts){
@@ -138,18 +139,18 @@
 		else{
 			$shoutHTML = 'No shouts available';
 		}
-    $html = loadTemplate('templates/shoutSystem',array(
+		$html = loadTemplate('templates/shoutSystem',array(
 			'shouts' => $shoutHTML
 		),true);
-    return $html;
-  }
-  function loadBattles($country){
-    $html = '';
-    return $html;
-  }
-  function loadNews(){
+		return $html;
+	}
+	function loadBattles($country){
+		$html = '';
+		return $html;
+	}
+	function loadNews(){
 		//Table needed
-    $query = sql('SELECT `title`,`votes`,`author`,`time` FROM :table ORDER BY `votes` DESC LIMIT 10','news');
+		$query = sql('SELECT `title`,`votes`,`author`,`time` FROM :table ORDER BY `votes` DESC LIMIT 10','news');
 		$html = '';
 		if($query){
 			foreach($query as $news){
@@ -167,12 +168,12 @@
 		else{
 			$html = 'No news available';
 		}
-    return $html;
-  }
-  function loadInfo(){
+		return $html;
+	}
+	function loadInfo(){
 		// Table needed
 		//$query = sql('SELECT `event`, `time` FROM :table LIMIT 10','events');
-    $html = '';
-    return $html;
-  }
+		$html = '';
+		return $html;
+	}
 ?>
