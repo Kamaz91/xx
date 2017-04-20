6b42b61fcd900225d1ef9e23cc7f22a06645bfa8
Damian Dygulski <dygulski13@gmail.com>
2017-04-20 21:36:48 +0200

    Fix a bug with company type loading

--- modules/myCompanies.php
+++ modules/myCompanies.php
@@ -1,26 +1,24 @@
 <?php
+	global $_SETTINGS;
+
 	$companies = sql('SELECT `id`,`name`,`type`,`level` FROM :table WHERE `owner` = :id','company',array(
 		'id' => $_SESSION['ID']
 	));
 	if($companies){
-		global $_SETTINGS;
-
 		$html = '';
 		foreach($companies as $c){
 			$html .= loadTemplate('templates/companyRow',$c,true);
 		}
-		$types = '';
-		foreach($_SETTINGS['companyTypes'] as $comp){
-			$types .= "<option value=$comp>$comp</option>";
-		}
-		loadTemplate('templates/myCompanies',array(
-			'companies' => $html,
-			'types' => $types
-		));
 	}
 	else{
-		loadTemplate('templates/myCompanies',array(
-			'companies' => 'You have no companies'
-		));
+		$html = 'You have no companies';
 	}
+	$types = '';
+	foreach($_SETTINGS['companyTypes'] as $comp){
+		$types .= "<option value=$comp>$comp</option>";
+	}
+	loadTemplate('templates/myCompanies',array(
+			'companies' => 'You have no companies',
+			'types' => $types
+		));
 ?>
