397d65068abce60ed96b36e61e5a738aef0f315f
Damian Dygulski <dygulski13@gmail.com>
2017-04-25 00:20:51 +0200

    Fix a bug that allowed to apply for a job even if you had one

--- modules/applyForWork.php
+++ modules/applyForWork.php
@@ -1,58 +1,63 @@
 <?php
-	if(!empty($_GET['id'])){
-		$offer = sql('SELECT * FROM :table WHERE `ID` = :id','jobOffers',array(
-			'id' => $_GET['id']
-		))[0];
-		if($offer){
-			if($_SESSION['eco'] >= $offer['skill']){
-				$work = sql('INSERT INTO :table (`usrid`,`salary`,`currency`) VALUES (:id,:salary,:currency)',$offer['company'].'_workers',array(
-					'id' =>	$_SESSION['ID'],
-					'salary' =>	$offer['salary'],
-					'currency' =>	$offer['currency']
-				));
-				$usr = sql('UPDATE :table SET `company` = :company WHERE `ID` = :id','users',array(
-					'company' => $offer['company'],
-					'id' => $_SESSION['ID']
-				));
-				if($work){
-					$count = $offer['quantity'] - 1;
-					if($count == 0){
-						$delete = sql('DELETE FROM :table WHERE `ID` = :id','jobOffers',array(
-							'id' => $_GET['id']
-						));
-						if($delete){
-							print 'done';
+	if($_SESSION['company'] == NULL){
+		if(!empty($_GET['id'])){
+			$offer = sql('SELECT * FROM :table WHERE `ID` = :id','jobOffers',array(
+				'id' => $_GET['id']
+			))[0];
+			if($offer){
+				if($_SESSION['eco'] >= $offer['skill']){
+					$work = sql('INSERT INTO :table (`usrid`,`salary`,`currency`) VALUES (:id,:salary,:currency)',$offer['company'].'_workers',array(
+						'id' =>	$_SESSION['ID'],
+						'salary' =>	$offer['salary'],
+						'currency' =>	$offer['currency']
+					));
+					$usr = sql('UPDATE :table SET `company` = :company WHERE `ID` = :id','users',array(
+						'company' => $offer['company'],
+						'id' => $_SESSION['ID']
+					));
+					if($work){
+						$count = $offer['quantity'] - 1;
+						if($count == 0){
+							$delete = sql('DELETE FROM :table WHERE `ID` = :id','jobOffers',array(
+								'id' => $_GET['id']
+							));
+							if($delete){
+								print 'done';
+							}
+							else{
+								error();
+							}
 						}
 						else{
-							error();
+							$update = sql('UPDATE :table SET `quantity` = :q WHERE `ID` = :id','jobOffers',array(
+								'q' => $count,
+								'id' => $_GET['id']
+							));
+							if($update){
+								print 'done';
+							}
+							else{
+								error();
+							}
 						}
 					}
 					else{
-						$update = sql('UPDATE :table SET `quantity` = :q WHERE `ID` = :id','jobOffers',array(
-							'q' => $count,
-							'id' => $_GET['id']
-						));
-						if($update){
-							print 'done';
-						}
-						else{
-							error();
-						}
+						error();
 					}
 				}
 				else{
-					error();
+					error('Your economy skill is lower than required');
 				}
 			}
 			else{
-				error('Your economy skill is lower than required');
+				error('Offer is no longer available');
 			}
 		}
 		else{
-			error('Offer is no longer available');
+			error();
 		}
 	}
 	else{
-		error();
+		error('You already have a job');
 	}
 ?>
