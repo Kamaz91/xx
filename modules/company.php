ef057df6c3514204b3b68bb0e0db9e4a3df813a9
Damian Dygulski <dygulski13@gmail.com>
2017-05-12 02:47:28 +0200

    company level loading

--- modules/company.php
+++ modules/company.php
@@ -2,21 +2,21 @@
 	global $_SETTINGS;
 
 	if(isset($_GET['id']) && !empty($_GET['id'])){
-		$query = sql('SELECT * FROM :table WHERE `id` = :id','company',array(
+		$company = sql('SELECT * FROM :table WHERE `id` = :id','company',array(
 			'id' => $_GET['id']
-		));
-		if($query){
+		))[0];
+		if($company){
 			$owner = sql('SELECT `nick` FROM :table WHERE `ID` = :id','users',array(
-				'id' => $query[0]['owner']
-			));
+				'id' => $company['owner']
+			))[0];
 			$workers = sql('SELECT `id`,`nick` FROM :table WHERE `company` = :company','users',array(
 				'company' => $_GET['id']
 			));
 			$infoHTML = loadTemplate('templates/companyInfo',array(
-				'owner' => $owner[0]['nick'],
-				'name' => $query[0]['name'],
-				'type' => $query[0]['type'],
-				'level' => $query[0]['level']
+				'owner' => $owner['nick'],
+				'name' => $company['name'],
+				'type' => $company['type'],
+				'level' => $company['level']
 			),1);
 			$workersHTML = '';
 			if($workers){
@@ -37,13 +37,14 @@
 				'companyInfo' => $infoHTML,
 				'workers' => $workersHTML
 			));
-			if($_SESSION['ID'] === $query[0]['owner']){
+			if($_SESSION['ID'] === $company['owner']){
 				$currencies = '';
 				foreach($_SETTINGS['currencyTypes'] as $c){
 					$currencies .= "<option value='$c'>$c</option>";
 				}
 				loadTemplate('templates/companyManagement',array(
 					'id' => $_GET['id'],
+					'level' => $company['level'],
 					'currencies' => $currencies
 				));
 			}
